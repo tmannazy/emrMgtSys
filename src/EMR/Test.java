56abfce8637dc7114b5419ad078d36a4ed01464e
OkoroPrecious <103753662+OkoroPrecious@users.noreply.github.com>
2022-06-21 23:31:18 +0100

    Hospital Test

--- src/EMR/Test.java
+++ src/EMR/Test.java
@@ -1,4 +1,50 @@
 package EMR;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Test {
-}
+
+        private String testName;
+        private double testCost;
+        private String testResult;
+        private LocalDateTime testDate;
+
+
+    public Test(String testName, double testCost, String testResult){
+            this.testName = testName;
+            this.testCost = testCost;
+            this.testResult = testResult;
+            testDate = LocalDateTime.now();
+
+
+        }
+
+        public void setTestName (String testName){
+            this.testName = testName;
+        }
+
+        public String getTestName () {
+            return testName;
+        }
+
+        public void setTestCost ( double testCost){
+            this.testCost = testCost;
+        }
+
+        public double getTestCost () {
+            return testCost;
+        }
+
+        public String getTestResult () {
+            return testResult;
+        }
+
+        public String getTestDate() {
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+            String dateCreated = testDate.format(dateFormatter);
+            return dateCreated;
+        }
+
+    }
